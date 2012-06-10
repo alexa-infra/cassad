d907a9389e768024e27d305da8c2911e907399d2
Alexey Vasilyev <alexa.infra@gmail.com>
2012-06-10 15:14:26 +0700

    refactor add path command

--- cassad/management/commands/add_path.py
+++ cassad/management/commands/add_path.py
@@ -9,9 +9,9 @@ from django.conf import settings
 thumbdir = getattr(settings, 'THUMBNAIL_DIR', None)
 tsize = 250
 
-from optparse import make_option
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 from mprogress import ProgressBar
+
 
 class Command(BaseCommand):
     help = 'Help text goes here'
@@ -23,31 +23,37 @@ class Command(BaseCommand):
     def shahash(self, filename):
         sha = hashlib.sha1()
         with open(filename, 'rb') as f:
-            for chunk in iter(lambda: f.read(128*sha.block_size), ''):
+            for chunk in iter(lambda: f.read(128 * sha.block_size), ''):
                 sha.update(chunk)
         return sha.hexdigest()
 
     def process_path(self, rootdir):
         count_files = 0
-        for root, subfolders, files in os.walk(rootdir):
+        for _, _, files in os.walk(rootdir):
             count_files += len(files)
         prog = ProgressBar(0, count_files, 77, mode='fixed')
-        oldprog = str(prog)
 
+        processed = 0
+        errors = 0
         for root, subfolders, files in os.walk(rootdir):
             for f in files:
                 filename = os.path.join(root, f)
                 try:
-                    if Picture.objects.filter(path=filename).count() == 0:
-                        self.process_file(filename)
+                    if self.process_file(filename):
+                        processed += 1
                 except:
                     print 'ERROR with %s' % filename
-                    pass
+                    print 'Exception', sys.exc_info()[0]
+                    errors += 1
                 prog.increment_amount()
                 print prog, "\r",
                 sys.stdout.flush()
+        print "\n"
+        print "total files: %d, added: %d, errors: %d \n" % (count_files, processed, errors)
 
     def process_file(self, filename):
+        if Picture.objects.filter(path=filename).count() != 0:
+            return False
         img = Image.open(filename)
         ts = os.path.getctime(filename)
         creation = datetime.fromtimestamp(ts)
@@ -66,22 +72,27 @@ class Command(BaseCommand):
 
         p.save()
 
+        try:
+            p.thumbnail = self.generate_thumbnail(img, filename, str(p.id))
+        except:
+            p.delete()
+            raise
+
+        p.save()
+        print filename, size, img.format, img.size, img.mode, creation, sha
+        return True
+
+    def generate_thumbnail(self, img, filename, thumbnailname):
         fname = os.path.basename(filename)
-        name, ext = os.path.splitext(fname)
-        name = str(p.id)
-        tname = os.path.join(thumbdir, name + ext)
+        _, ext = os.path.splitext(fname)
+        tname = os.path.join(thumbdir, thumbnailname + ext)
 
         w, h = img.size
         factor = min(tsize / float(w), tsize / float(h))
-        w = int(w * factor)
-        h = int(h * factor)
-
-        mode = img.format
+        w, h = int(w * factor), int(h * factor)
         s = (w, h)
+
         thumbnail = img.resize(s, Image.ANTIALIAS)
-        thumbnail.save(tname, mode)
+        thumbnail.save(tname, img.format)
 
-        p.thumbnail = tname
-        p.save()
-
-        print filename, size, img.format, img.size, img.mode, creation, sha
+        return tname
